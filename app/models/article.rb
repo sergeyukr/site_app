18baa6af2a9ddffb1abdea7ed5bf4c0c5e89aa13
sergeyukr <s.a.klymenko@gmail.com>
2012-12-21 01:03:36 +0200

    Index redy

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
 	validates :name, :length => { :minimum => 1 }
 	validates :content, :length => { :minimum => 10 }
+	validates :category, :length => { :minimum => 1 }
 end
