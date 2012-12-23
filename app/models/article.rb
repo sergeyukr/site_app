ded12f79ea30b5e3df23fdb3ac561dad26cb0136
sergeyukr <s.a.klymenko@gmail.com>
2012-12-23 23:43:36 +0200

    Ready

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
-class Article < ActiveRecord::Base
-	validates :name, :length => { :in => 5..40 }
-	validates :content, :length => { :minimum => 10 }
-	validates :categoriy, :length => { :minimum => 1 }
+﻿class Article < ActiveRecord::Base
+	validates :name, :length => { :in => 5..60, :message => "Допустимое количество символов заголовка от 5 до 40" } 
+	
+	validates :content, :length => { :minimum => 10, :message => "Ваша статья слишком мала" }
+	validates :categoriy, :length => { :minimum => 1, :message => "Присвойте статье категорию" }
 end
